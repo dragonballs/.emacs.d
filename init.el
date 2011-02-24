b0a2265af50ab024aa32d73cf9bcad27eeba79fc
Keren Dong <dongkeren@gmail.com>
2011-02-24 21:20:17 +0000

    good init

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;;
 ;; This is the first thing to get loaded.
 ;;
+(when (eq window-system 'ns) (toggle-scroll-bar -1))
 
 ;; org-mode windmove compatibility
 (setq org-replace-disputed-keys t)
@@ -20,13 +21,13 @@
 ;; Package Locations
 ;; Location of various local packages (in .emacs.d/vendor or .emacs.d/src)
 ;;  because I don't want to keep them in =/Applications/Emacs.app/= or in
-;;  =/usr/share/local/=. 
+;;  =/usr/share/local/=.
 
 (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-        (let* ((my-lisp-dir "~/.emacs.d/")
-              (default-directory my-lisp-dir))
-           (setq load-path (cons my-lisp-dir load-path))
-           (normal-top-level-add-subdirs-to-load-path)))
+    (let* ((my-lisp-dir "~/.emacs.d/")
+           (default-directory my-lisp-dir))
+      (setq load-path (cons my-lisp-dir load-path))
+      (normal-top-level-add-subdirs-to-load-path)))
 
 ;; Load up Org Mode and Babel
 (require 'org-install)
