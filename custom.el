c2e9a1e3efaf19ce5f6154a639f81ae486e9d6ac
Keren Dong <dongkeren@gmail.com>
2011-02-25 16:25:51 +0000

    customize ecb

---
+++ custom.el
@@ -0,0 +1,39 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-view-program-list (quote (("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %n %o %b"))))
+ '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Skim") (output-html "xdg-open"))))
+ '(blink-cursor-mode nil)
+ '(ecb-compilation-buffer-names (quote (("*Calculator*") ("*vc*") ("*vc-diff*") ("*vc-.**" . t) ("*Apropos*") ("*Occur*") ("*shell*") ("*ansi-term*") ("\\*[cC]ompilation.*\\*" . t) ("\\*i?grep.*\\*" . t) ("*JDEE Compile Server*") ("*Help*") ("*Completions*") ("*Backtrace*") ("*Compile-log*") ("*bsh*") ("*Messages*") ("*eshell*") ("*R*") ("*Ido Completions*") ("*ess-output*") ("*Tex Help*") ("Tex Live 2010") ("*Tex.**" . t) ("*ESS.*" . t) ("*Customize.**" . t) ("*help.**" . t) ("*Process List*"))))
+ '(ecb-compile-window-height 0.25)
+ '(ecb-compile-window-temporally-enlarge (quote both))
+ '(ecb-compile-window-width (quote edit-window))
+ '(ecb-eshell-buffer-sync (quote always))
+ '(ecb-eshell-buffer-sync-delay nil)
+ '(ecb-layout-name "left6")
+ '(ecb-layout-window-sizes (quote (("left9" (0.11049723756906077 . 1.302325581395349)) ("left14" (0.17679558011049723 . 0.9767441860465116) (0.17679558011049723 . 0.32558139534883723)) ("left6" (0.19337016574585636 . 0.42105263157894735) (0.19337016574585636 . 0.2807017543859649) (0.19337016574585636 . 0.2807017543859649)) ("left8" (0.14917127071823205 . 0.2807017543859649) (0.14917127071823205 . 0.24561403508771928) (0.14917127071823205 . 0.2807017543859649) (0.14917127071823205 . 0.17543859649122806)) ("left-analyse" (0.24581005586592178 . 0.2807017543859649) (0.24581005586592178 . 0.24561403508771928) (0.24581005586592178 . 0.22807017543859648) (0.24581005586592178 . 0.22807017543859648)))))
+ '(ecb-options-version "2.40")
+ '(ecb-tip-of-the-day nil)
+ '(ecb-use-speedbar-instead-native-tree-buffer (quote source))
+ '(ecb-windows-height 0.25)
+ '(ecb-windows-width 0.25)
+ '(eshell-scroll-to-bottom-on-output this)
+ '(g-user-email "dongkeren@gmail.com")
+ '(imaxima-bg-color nil)
+ '(imaxima-equation-color "Black")
+ '(imaxima-fg-color nil)
+ '(imaxima-fnt-size "huge")
+ '(imaxima-scale-factor 1.0)
+ '(imaxima-use-maxima-mode-flag t t)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (text-mode-hook-identify))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((default nil) (nil nil)))
+ '(font-lock-function-name-face ((t (:foreground "aquamarine" :weight bold :height 1))))
+ '(variable-pitch ((default nil) (nil nil))))
